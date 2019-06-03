18cc3af36cee0f7b35e25d083b96b595dd9e518b
华丽丽 <806536938@qq.com>
2019-06-03 10:49:16 +0800

    7.reverseInteger

---
+++ src/ReverseInteger.java
@@ -0,0 +1,43 @@
+//7.Given a 32-bit signed integer, reverse digits of an integer.
+//Example 1:
+//Input: 123
+//Output: 321
+
+//Example 2:
+//Input: -123
+//Output: -321
+
+//Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range:
+// [−231,  231 − 1].
+// For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+//Example 3:
+//Input: 120
+//Output: 21
+public class ReverseInteger {
+    public static void main(String[] args){
+        int x  = -123;
+        int ret;
+        ret = Reverse(x);
+        System.out.println(ret);
+    }
+
+    //看的讨论区，最简单的一个方法
+    //If overflow exists, the new result will not equal previous one.
+    //No flags needed. No hard code like 0xf7777777 needed.
+    private static int Reverse(int x) {
+        int ret = 0;
+        while (x != 0){
+            int tail = x % 10;
+            int newret = ret * 10 + tail;
+            if ((newret - tail)/10 != ret){
+                return 0;
+            }
+            ret = newret;
+            x = x / 10;
+        }
+
+        return ret;
+    }
+
+}
