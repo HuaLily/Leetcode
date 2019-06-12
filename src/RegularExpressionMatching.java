de27fa86d48a06c6452e7ab29fd4ade9a9ffc61a
华丽丽 <806536938@qq.com>
2019-06-12 14:39:42 +0800

    10

--- src/RegularExpressionMatching.java
+++ src/RegularExpressionMatching.java
@@ -5,8 +5,8 @@
 //'*' Matches zero or more of the preceding element.
 public class RegularExpressionMatching {
     public static void main(String[] args) {
-        String s = "aab";
-        String p = "aa.";
+        String s = "aabb";
+        String p = "aa.b*";
         boolean ret = isMatch(s,p);
         System.out.println(ret);
     }
@@ -29,11 +29,22 @@ public class RegularExpressionMatching {
             //如果pattern是空的话，就看text是不是空
             return text.isEmpty();
         }
+
         //判断text不为空的情况，是不是相同字符，或者是'.'
         boolean first_match = (!text.isEmpty())&&
                 (text.charAt(0) == pattern.charAt(0)||
                 pattern.charAt(0) == '.');
-        return first_match && isMatch(text.substring(1),pattern.substring(1));
+
+        //Then, we may ignore this part of the pattern,
+        // or delete a matching character in the text.
+        // If we have a match on the remaining strings after any of these operations,
+        // then the initial inputs matched.
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
+            return (isMatch(text,pattern.substring(2)) ||
+                    (first_match && isMatch(text.substring(1),pattern)));
+        }else{
+            return first_match && isMatch(text.substring(1),pattern.substring(1));
+        }
 
     }
 
