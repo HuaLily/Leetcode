eac00cb50f15730de9e78cbbff122a344d2cff2e
华丽丽 <806536938@qq.com>
2019-06-14 09:13:31 +0800

    11. Container With Most Water 暴力破解+时间复杂度为O（n）的优化算法

--- src/ContainerWithMostWater.java
+++ src/ContainerWithMostWater.java
@@ -3,12 +3,15 @@
 // n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
 // Find two lines, which together with x-axis forms a container, such that the container contains the most water.
 
+import java.util.jar.JarEntry;
+
 public class ContainerWithMostWater {
     public static void main(String[] args) {
         int[] input = new  int[]{1,8,6,2,5,4,8,3,7};
-        int ret = maxArea(input);
+        int ret = maxArea2(input);
         System.out.println(ret);
     }
+
 
     //首先想到的思路是：暴力破解,
     //Runtime: 205 ms, faster than 18.41% of Java online submissions for Container With Most Water.
@@ -27,4 +30,25 @@ public class ContainerWithMostWater {
         }
         return max_area;
     }
+
+    //第二种，设置两个指针，分别指向数组两端，向内计算面积的同时，保留长的数据，改变短的数据
+    //比暴力破解运算减少一轮，最多计算n次
+    //Runtime: 2 ms, faster than 95.45% of Java online submissions for Container With Most Water.
+    //Memory Usage: 39.5 MB, less than 95.14% of Java online submissions for Container With Most Water.
+    private static int maxArea2(int[] height) {
+        int max_area = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right){
+            max_area = Math.max(max_area,Math.min(height[left],height[right])*(right - left));
+            if (height[left] < height[right]){
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return max_area;
+    }
+
+
 }
