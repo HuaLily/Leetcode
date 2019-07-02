40139e4fee52a5bb4cfb6d465c28d606fe725fa9
华丽丽 <806536938@qq.com>
2019-07-02 15:47:52 +0800

    14.Longest Common Prefix

---
+++ src/collection.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class collection {
+    public static void main(String[] args) {
+       String s = "3";
+        System.out.println(s.substring(1));
+
+    }
+}
