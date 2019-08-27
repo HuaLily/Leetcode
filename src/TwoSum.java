ed528abf5c8417a00951a8256e89752d5e18dc33
wenli.hua <huawenli345@gmail.com>
2019-08-27 10:03:33 +0800

    增加了暴力破解的简单方法，和时间复杂度

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -8,11 +8,29 @@ public class TwoSum {
         int[] nums ={3,2,4};
         int target = 6;
         //int[] answer = twosum2(nums,target);
-        int[] answer = twosum1(nums,target);
+        int[] answer = twosum(nums,target);
         System.out.println(Arrays.toString(answer));
     }
 
-    //两次for
+    //暴力破解的方法
+    //执行用时 : 70 ms
+    //内存消耗 : 38.9 MB  时间复杂度为O(n^2)
+    private static int[] twosum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    //两次for 使用map
+    //时间复杂度为O(n^2) 执行用时：5 ms
     private static int[] twosum2(int[] nums, int target) {
 
         Map<Integer, Integer> map = new HashMap<>();
@@ -33,7 +51,6 @@ public class TwoSum {
     }
 
     //一次for
-
     private static int[] twosum1(int[] nums,int target){
         Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
